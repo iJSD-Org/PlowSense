467768f1fb0121bce5dd92513b6ae3231893d8e1
MiguelRobles7 <donnaldmiguelrobles7@gmail.com>
2020-08-19 14:50:53 +0800

    Add Temporary Charts in Statistics.cs

--- PlowSense/Statistics.Designer.cs
+++ PlowSense/Statistics.Designer.cs
@@ -31,19 +31,21 @@
             this.components = new System.ComponentModel.Container();
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2Separator1 = new Guna.UI2.WinForms.Guna2Separator();
             this.guna2ShadowPanel2 = new Guna.UI2.WinForms.Guna2ShadowPanel();
             this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
-            this.pieChart1 = new LiveCharts.WinForms.PieChart();
+            this.statsPie = new LiveCharts.WinForms.PieChart();
             this.guna2ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
-            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
-            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            this.guna2Separator1 = new Guna.UI2.WinForms.Guna2Separator();
+            this.statsChart = new LiveCharts.WinForms.CartesianChart();
+            this.guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
             this.panel1.SuspendLayout();
             this.guna2ShadowPanel2.SuspendLayout();
             this.guna2ShadowPanel1.SuspendLayout();
             this.guna2Panel1.SuspendLayout();
+            this.guna2Panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // guna2Elipse1
@@ -52,17 +54,37 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.guna2Panel3);
             this.panel1.Controls.Add(this.guna2HtmlLabel1);
             this.panel1.Controls.Add(this.guna2Separator1);
             this.panel1.Controls.Add(this.guna2ShadowPanel2);
             this.panel1.Controls.Add(this.guna2ShadowPanel1);
             this.panel1.Controls.Add(this.guna2ComboBox1);
-            this.panel1.Controls.Add(this.cartesianChart1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(912, 548);
             this.panel1.TabIndex = 10;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(105)))), ((int)(((byte)(54)))));
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(12, 12);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(117, 57);
+            this.guna2HtmlLabel1.TabIndex = 8;
+            this.guna2HtmlLabel1.Text = "Stats";
+            // 
+            // guna2Separator1
+            // 
+            this.guna2Separator1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(205)))), ((int)(((byte)(5)))));
+            this.guna2Separator1.FillThickness = 2;
+            this.guna2Separator1.Location = new System.Drawing.Point(12, 65);
+            this.guna2Separator1.Name = "guna2Separator1";
+            this.guna2Separator1.Size = new System.Drawing.Size(235, 10);
+            this.guna2Separator1.TabIndex = 9;
             // 
             // guna2ShadowPanel2
             // 
@@ -80,7 +102,7 @@
             // 
             // guna2Panel2
             // 
-            this.guna2Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(226)))), ((int)(((byte)(194)))));
+            this.guna2Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(222)))), ((int)(((byte)(186)))));
             this.guna2Panel2.Location = new System.Drawing.Point(6, 3);
             this.guna2Panel2.Margin = new System.Windows.Forms.Padding(2);
             this.guna2Panel2.Name = "guna2Panel2";
@@ -104,23 +126,23 @@
             // 
             // guna2Panel1
             // 
-            this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(226)))), ((int)(((byte)(194)))));
-            this.guna2Panel1.Controls.Add(this.pieChart1);
+            this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(222)))), ((int)(((byte)(186)))));
+            this.guna2Panel1.Controls.Add(this.statsPie);
             this.guna2Panel1.Location = new System.Drawing.Point(6, 3);
             this.guna2Panel1.Margin = new System.Windows.Forms.Padding(2);
             this.guna2Panel1.Name = "guna2Panel1";
             this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
-            this.guna2Panel1.Size = new System.Drawing.Size(200, 171);
+            this.guna2Panel1.Size = new System.Drawing.Size(286, 235);
             this.guna2Panel1.TabIndex = 0;
             // 
-            // pieChart1
+            // statsPie
             // 
-            this.pieChart1.Location = new System.Drawing.Point(8, 2);
-            this.pieChart1.Margin = new System.Windows.Forms.Padding(2);
-            this.pieChart1.Name = "pieChart1";
-            this.pieChart1.Size = new System.Drawing.Size(278, 233);
-            this.pieChart1.TabIndex = 2;
-            this.pieChart1.Text = "pieChart1";
+            this.statsPie.Location = new System.Drawing.Point(8, 2);
+            this.statsPie.Margin = new System.Windows.Forms.Padding(2);
+            this.statsPie.Name = "statsPie";
+            this.statsPie.Size = new System.Drawing.Size(276, 231);
+            this.statsPie.TabIndex = 2;
+            this.statsPie.Text = "pieChart1";
             // 
             // guna2ComboBox1
             // 
@@ -142,34 +164,25 @@
             this.guna2ComboBox1.Size = new System.Drawing.Size(134, 26);
             this.guna2ComboBox1.TabIndex = 12;
             // 
-            // cartesianChart1
+            // statsChart
             // 
-            this.cartesianChart1.Location = new System.Drawing.Point(12, 80);
-            this.cartesianChart1.Margin = new System.Windows.Forms.Padding(2);
-            this.cartesianChart1.Name = "cartesianChart1";
-            this.cartesianChart1.Size = new System.Drawing.Size(583, 244);
-            this.cartesianChart1.TabIndex = 11;
-            this.cartesianChart1.Text = "z";
+            this.statsChart.Location = new System.Drawing.Point(20, 8);
+            this.statsChart.Margin = new System.Windows.Forms.Padding(2);
+            this.statsChart.Name = "statsChart";
+            this.statsChart.Size = new System.Drawing.Size(550, 225);
+            this.statsChart.TabIndex = 11;
+            this.statsChart.Text = "z";
             // 
-            // guna2HtmlLabel1
+            // guna2Panel3
             // 
-            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(105)))), ((int)(((byte)(54)))));
-            this.guna2HtmlLabel1.Location = new System.Drawing.Point(12, 12);
-            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
-            this.guna2HtmlLabel1.Size = new System.Drawing.Size(117, 57);
-            this.guna2HtmlLabel1.TabIndex = 8;
-            this.guna2HtmlLabel1.Text = "Stats";
-            // 
-            // guna2Separator1
-            // 
-            this.guna2Separator1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(205)))), ((int)(((byte)(5)))));
-            this.guna2Separator1.FillThickness = 2;
-            this.guna2Separator1.Location = new System.Drawing.Point(12, 65);
-            this.guna2Separator1.Name = "guna2Separator1";
-            this.guna2Separator1.Size = new System.Drawing.Size(235, 10);
-            this.guna2Separator1.TabIndex = 9;
+            this.guna2Panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(222)))), ((int)(((byte)(186)))));
+            this.guna2Panel3.Controls.Add(this.statsChart);
+            this.guna2Panel3.Location = new System.Drawing.Point(12, 83);
+            this.guna2Panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.guna2Panel3.Name = "guna2Panel3";
+            this.guna2Panel3.ShadowDecoration.Parent = this.guna2Panel3;
+            this.guna2Panel3.Size = new System.Drawing.Size(587, 241);
+            this.guna2Panel3.TabIndex = 2;
             // 
             // Statistics
             // 
@@ -179,7 +192,7 @@
             this.ClientSize = new System.Drawing.Size(912, 548);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Statistics";
             this.Text = "Statistics";
             this.Load += new System.EventHandler(this.Statistics_Load);
@@ -188,6 +201,7 @@
             this.guna2ShadowPanel2.ResumeLayout(false);
             this.guna2ShadowPanel1.ResumeLayout(false);
             this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -200,10 +214,11 @@
 		private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
 		private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
 		private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
-		private LiveCharts.WinForms.PieChart pieChart1;
+		private LiveCharts.WinForms.PieChart statsPie;
 		private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox1;
-		private LiveCharts.WinForms.CartesianChart cartesianChart1;
+		private LiveCharts.WinForms.CartesianChart statsChart;
 		private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
 		private Guna.UI2.WinForms.Guna2Separator guna2Separator1;
+		private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
 	}
 }
--- PlowSense/Statistics.cs
+++ PlowSense/Statistics.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Windows.Media;
+using System.Collections.Generic;
 using System.Windows.Forms;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace PlowSense
 {
@@ -9,20 +13,76 @@ namespace PlowSense
         {
             InitializeComponent();
         }
-
+		BrushConverter converter = new System.Windows.Media.BrushConverter();
+		Func<ChartPoint, string> labelPoint = chartpoint => string.Format("{1:P}", chartpoint.Y, chartpoint.Participation);
 		private void Statistics_Load(object sender, EventArgs e)
 		{
-
+			PieLoad();
 		}
-
-		private void pieChart1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+		void LineLoad()
 		{
-
+			statsChart.Series = new SeriesCollection
+			{
+				new LineSeries
+				{
+					Title = "Line 1",
+					Values = new ChartValues<int> {61, 63, 37, 62, 67, 75, 45, 52, 54, 45},
+					Fill = new SolidColorBrush(System.Windows.Media.Color.FromArgb(80,9, 105, 54)),
+					PointGeometry = null,
+					LineSmoothness = 100,
+					Stroke = new SolidColorBrush(System.Windows.Media.Color.FromRgb(9, 105, 54))
+				},
+			};
+			List<string> l = new List<string> { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+			statsChart.AxisX.Add(new Axis
+			{
+				Title = "X",
+				Labels = l
+			});
+			statsChart.AxisY.Add(new Axis
+			{
+				Title = "Y",
+				MinValue = 0
+			});
 		}
-
-		private void riceToolStripMenuItem_Click(object sender, EventArgs e)
+		void PieLoad()
 		{
-
+			statsPie.Series= new SeriesCollection
+			{
+				new PieSeries
+				{
+					Title = "Title 1",
+					Values = new ChartValues<double> {100},
+					Fill = (System.Windows.Media.Brush)converter.ConvertFromString("#9CE74C"),
+					DataLabels = true,
+					LabelPoint = labelPoint,
+					StrokeThickness = 0,
+				},
+				new PieSeries
+				{
+					Title = "Title 2",
+					Values = new ChartValues<double> {300},
+					Fill = (System.Windows.Media.Brush)converter.ConvertFromString("#DECD05"),
+					DataLabels = true,
+					LabelPoint = labelPoint,
+					StrokeThickness = 0
+				},
+				new PieSeries
+				{
+					Title = "Title 3",
+					Values = new ChartValues<double> {250},
+					Fill = (System.Windows.Media.Brush)converter.ConvertFromString("#BBB324"),
+					DataLabels = true,
+					LabelPoint = labelPoint,
+					StrokeThickness = 0
+				},
+			};
+			DefaultLegend customLegend = new DefaultLegend();
+			customLegend.BulletSize = 10;
+			customLegend.Foreground = System.Windows.Media.Brushes.White;
+			customLegend.FontSize = 15;
+			statsPie.DefaultLegend = customLegend;
+			statsPie.LegendLocation = LegendLocation.Right;
 		}
 	}
 }
