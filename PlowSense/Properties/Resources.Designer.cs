edede14d0d2712f3cc0e86c23b09f924a594b5e1
sitiom <56180050+sitiom@users.noreply.github.com>
2020-08-21 02:17:58 +0000

    Add refresh icon

--- PlowSense/Properties/Resources.Designer.cs
+++ PlowSense/Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace PlowSense.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap grefresh_480px {
+            get {
+                object obj = ResourceManager.GetObject("grefresh_480px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_exit_208px {
             get {
                 object obj = ResourceManager.GetObject("icons8_exit_208px", resourceCulture);
